de6b7f7b22a509410c6852d0a40849505bbf1dfa
js.lee <js7311.lee@samsung.com>
2018-11-01 12:34:46 +0900

    change subscribe method

--- src/main/java/team/three/msgconsumer/manager/arch/kafka/KafkaWorkerII.java
+++ src/main/java/team/three/msgconsumer/manager/arch/kafka/KafkaWorkerII.java
@@ -1,10 +1,12 @@
 package team.three.msgconsumer.manager.arch.kafka;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
-import java.time.Duration;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -12,11 +14,17 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import team.three.msgconsumer.manager.arch.TaskManager;
 import team.three.msgconsumer.manager.config.ConfigManager;
+import team.three.msgconsumer.props.IdMaker;
 
 public class KafkaWorkerII extends Thread {
 	public void run() {
 		KafkaConsumer<String, byte[]> consumer = null;
 		TaskManager tm = TaskManager.get();
+		List<String> topicArr = new ArrayList<>();
+		for( int i=ConfigManager.get().getEqpEIdx(); i<=ConfigManager.get().getEqpEIdx(); i++) {
+			topicArr.add(IdMaker.makeEqpId(i));
+		}
+		
 		try {
 			Properties props = new Properties();
 			Map<String, String> cMap = ConfigManager.get().getIndiv();
@@ -28,8 +36,8 @@ public class KafkaWorkerII extends Thread {
 		    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		    props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
 		    consumer = new KafkaConsumer<>(props);
-		    Pattern ptn = Pattern.compile("EQP-*");
-		    consumer.subscribe(ptn);
+		    //Pattern ptn = Pattern.compile("EQP-*");
+		    consumer.subscribe(topicArr);
 		    while (true) {
 		        ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofMillis(100));
 		        for (ConsumerRecord<String, byte[]> record : records)
